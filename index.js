4d7d1311ee785cbadc10707f68e14034f089e289
MangoV3 <53501324+MangoV3@users.noreply.github.com>
2024-02-12 20:36:05 -0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     // Display confetti animation
-    document.getElementById('confetti').innerHTML = '<img src="confetti.gif">';
+    document.getElementById('confetti').innerHTML = '<img src="e5833e1bea7d379f0f4e4ae250b7cf81.gif">';
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
