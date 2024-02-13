79ae0291040f4937bf3529777c19bb235df84364
MangoV3 <53501324+MangoV3@users.noreply.github.com>
2024-02-13 12:53:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
+    if (event.target.id == 'yesBtn'){
     window.location.href = 'https://en.wikipedia.org/wiki/Heart_symbol'; // Redirect to Wikipedia page if "Yes" button is clicked
+    }
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
