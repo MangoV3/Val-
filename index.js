6abff3e55bc5cfa6529b0b9404d20c10e89b3c12
MangoV3 <53501324+MangoV3@users.noreply.github.com>
2024-02-13 12:38:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.getElementById('yes').addEventListener('click', function() {
+document.getElementById('yesBtn').addEventListener('click', function() {
+    if (event.target.id === 'yesBtn') {
     window.location.href = 'https://en.wikipedia.org/wiki/Heart_symbol'; // Redirect to Wikipedia page if "Yes" button is clicked
+    }
 });
 
-document.getElementById('no').addEventListener('click', function() {
+document.getElementById('noBtn').addEventListener('click', function() {
     window.location.href = 'https://www.istockphoto.com/illustrations/thumbs-down-clipart'; // Redirect to iStockphoto page if "No" button is clicked
 });
